fcd53680eb3dd0e2fa6e18e5f165e86c725ede21
Yuxiang Chen <yuxiangc@iastate.edu>
2016-10-12 18:19:41 -0500

    Finish functionality of admin

--- CS319-lab4/lib/booksLibrary.js
+++ CS319-lab4/lib/booksLibrary.js
@@ -13,7 +13,30 @@ function Library(username) {
     this.shelfs.push(s2);
     this.shelfs.push(s3);
     this.shelfs.push(s4);
+
+    for (var i = 0; i < 25; i++) {
+        var id = Math.floor(Math.random() * 1000);
+        var b;
+        if (i % 5 == 0) {
+            var name = "Ref " + (Math.floor(Math.random() * 25) + 1);
+            b = new Book(name, id, true, null, true);
+        } else {
+            var name = "Book " + (Math.floor(Math.random() * 25) + 1);
+            b = new Book(name, id, false, null, true);
+        }
+
+        if (id % 4 == 0) {
+            this.shelfs[0].books.push(b);
+        } else if (id % 4 == 1) {
+            this.shelfs[1].books.push(b);
+        } else if (id % 4 == 2) {
+            this.shelfs[2].books.push(b);
+        } else if (id % 4 == 3) {
+            this.shelfs[3].books.push(b);
+        }
+    }
 }
+
 
 Library.prototype.createTable = function () {
 
@@ -21,22 +44,34 @@ Library.prototype.createTable = function () {
         var rowIndex = 0;
         var colIndex = 0;
 
+        var mytable = $("<table id='table' border='2'></table>"); // creates DOM elements
+        var mytablebody = $('<tbody></tbody>');
+        var curr_row = $('<tr></tr>');
+        for (var i = 0; i < 4; i++) {
+            var curr_cell = $('<th width="80" height="18"></th>');
+            curr_cell.append(l.shelfs[i].name);
+            curr_row.append(curr_cell);
+            mytablebody.append(curr_row);
+        }
+
         if (window.localStorage.getItem("shelfs") === null) {
             console.log("no lib yet");
-            var mytable = $("<table id='table' border='2'></table>"); // creates DOM elements
-            var mytablebody = $('<tbody></tbody>');
-            var curr_row = $('<tr></tr>');
-            for (var i = 0; i < 4; i++) {
-                var curr_cell = $('<th width="80" height="18"></th>');
-                curr_cell.append(l.shelfs[i].name);
-                curr_row.append(curr_cell);
-                mytablebody.append(curr_row);
+
+            var maxRows = 0;
+            for (i = 0; i < 4; i++) {
+                if (l.shelfs[i].books.length > maxRows) {
+                    maxRows = l.shelfs[i].books.length;
+                }
             }
-            for (var j = 0; j < 10; j++) {
+
+            for (var j = 0; j < maxRows; j++) {
 
                 curr_row = $('<tr></tr>');
                 for (i = 0; i < 4; i++) {
                     var curr_cell = $('<td class="book" width="80" height="18"></td>');
+                    if (l.shelfs[i].books[j] != undefined) {
+                        curr_cell.append(l.shelfs[i].books[j].bookName);
+                    }
                     curr_row.append(curr_cell);
                 }
                 mytablebody.append(curr_row);
@@ -44,9 +79,12 @@ Library.prototype.createTable = function () {
             mytable.append(mytablebody);
             mytable.insertAfter($('#lib'));
 
-
+            var shelfString = JSON.stringify(l.shelfs);
+            localStorage.setItem("shelfs", shelfString);
         } else {
             //TODO
+            l.shelfs = JSON.parse(localStorage.getItem("shelfs"));
+
         }
 
         $("#table td").click(function () {
@@ -59,6 +97,10 @@ Library.prototype.createTable = function () {
             if (value === "" && /^username=admin/.test(document.cookie)) {
                 console.log("Add book here");
                 $("#form").show();
+            } else if (value != "") {
+                var book = l.shelfs[colIndex].books[rowIndex - 1];
+                var des = "Book name: " + book.bookName + " Book ID: " + book.bookID + "\nIs Reference: " + book.isReference + " Borrowed By: " + book.brrowedBy + " Present: " + book.presence;
+                $("#description").html(des);
             }
 
         });
@@ -74,19 +116,13 @@ Library.prototype.createTable = function () {
             var id = $("#bookID").val();
             var newBook = new Book(name, id, false, null, true);
 
-            if (parseInt(id) % 4 === 0) {
-                l.shelfs[0].books.push(newBook);
-            } else if (parseInt(id) % 4 === 1) {
-                l.shelfs[1].books.push(newBook);
-            } else if (parseInt(id) % 4 === 2) {
-                l.shelfs[2].books.push(newBook);
-            } else if (parseInt(id) % 4 === 3) {
-                l.shelfs[3].books.push(newBook);
-            }
+            l.shelfs[colIndex].books.push(newBook);
             console.log("add to R: " + rowIndex + " C: " + colIndex);
-            document.getElementById("table").children[0].children[rowIndex].children[colIndex].innerHTML += newBook.bookName;
+            document.getElementById("table").children[0].children[l.shelfs[colIndex].books.length].children[colIndex].innerHTML += newBook.bookName;
+            $("#form").hide();
+            $("#bookName").val("");
+            $("#bookID").val("");
         });
-
     } // end of createTable
 
 
