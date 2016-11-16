a74db388c8a3d1c999851209a7355e0795ee151a
chen8160 <chenyuxiang8160@gmail.com>
2016-11-15 22:55:51 -0600

    Login page works now

--- CS319-lab7/lib/app.js
+++ CS319-lab7/lib/app.js
@@ -1,14 +1,33 @@
-angular.module('myApp', ['ngRoute']).config(function ($routeProvider) {
-    $routeProvider.when("/login", {
-        //TODO: controller
-        templateUrl: "login.html"
+angular.module('myApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider.when("/login", {
+            //TODO: controller
+            controller: "loginController",
+            templateUrl: "login.html"
+        });
+        $routeProvider.when("/student", {
+            //TODO: controller
+            templateUrl: "student.html"
+        });
+        $routeProvider.when("/librarian", {
+            //TODO: controller
+            templateUrl: "librarian.html"
+        });
+    })
+    .controller('indexController', function ($scope) {})
+    .controller('loginController', function ($scope) {
+        $scope.url = "#login";
+        $scope.loginFn = function () {
+            if ($scope.username == "admin" && $scope.username == $scope.password) {
+                $scope.url = "#librarian";
+            } else if (/^U/.test($scope.username)) {
+                $scope.url = "#student";
+            } else {
+                $scope.url = "#login";
+            }
+        }
+        $scope.checkLogin = function () {
+            if (/^U/.test($scope.username) || $scope.username == "admin" && $scope.username == $scope.password) return;
+            alert("Login Failed");
+        }
     });
-    $routeProvider.when("/student", {
-        //TODO: controller
-        templateUrl: "student.html"
-    });
-    $routeProvider.when("/librarian", {
-        //TODO: controller
-        templateUrl: "librarian.html"
-    });
-}).controller('indexController', function ($scope) {});
