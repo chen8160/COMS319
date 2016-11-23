4cd26bbce24fd2d25fb206701926db34c1659731
Yuxiang Chen <yuxiangc@iastate.edu>
2016-11-22 21:20:34 -0600

    finish email

--- CS319-lab8/lab8/xml.g4
+++ CS319-lab8/lab8/xml.g4
@@ -1,33 +1,46 @@
 lexer grammar xml;
 
 @lexer::members {
-  int total = 0;
+  boolean tag = false;
+  String tagName = "";
 }
 
 fragment DIGIT: [0123456789];
 fragment ALPHA: [a-zA-Z];
-fragment ALPHANUM: (DIGIT | ALPHA)+;
-fragment SPECIALCHAR: ('-' | '_' | '~' | '!' | '$' | '&' | ''' | '(' | ')' | '*' | '+' | ',' | ';' | '=' | ':' );
+//fragment ALPHANUM: (DIGIT | ALPHA)+;
+fragment SPECIALCHAR: [-_~!$&'()*+,;=:];
 
 
+//3.1
 fragment ELEMENTNAME: (('x' | 'X' 'm' | 'M' ((DIGIT | '_' | '-' | '.' | [a-kA-Km-zM-Z]) (DIGIT | '_' | '-' | '.' | ALPHA)*)? ) | (('x' | 'X') ((DIGIT | '_' | '-' | '.' | [a-lA-Ln-zN-Z]) (DIGIT | '_' | '-' | '.' | ALPHA)*)? ) | (([a-wA-Wy-zY-Z] | '_') (DIGIT | '_' | '-' | '.' | ALPHA)*));
 
-fragment LOCAL: (ALPHANUM | SPECIALCHAR)
+//3.2
+fragment LOCAL: (DIGIT | ALPHA | SPECIALCHAR) ((DIGIT | ALPHA | SPECIALCHAR) | '.' (DIGIT | ALPHA | SPECIALCHAR))*;
+fragment DOMAIN: [0-9a-zA-Z.]+;
+
+//3.3
+fragment DATE: [1-9] | [1-2] [0-9] | [3] [01];
+//fragment MONTH:
+
+STARTING: '</' {tag = true;} | '<' {tag = true;};
+STOPPING: '>' {tag = false;};
 
 
 
-ELEMENTTAG: '<' ELEMENTNAME? '>' {
+ELEMENTTAG: {tag}? ELEMENTNAME {
 
-  String output = getText().substring(1, getText().length() - 1);
-  System.out.println("Starting Element Name: " + output);
-
-}
-| '</' ELEMENTNAME? '>' {
-
-  output = getText().substring(2, getText().length() - 1);
-  System.out.println("Stopping Element Name: " + output);
+  tagName = getText();
+  System.out.println("Element Name: " + getText());
 
 };
+
+EMAIL: {!tag && tagName.matches("(?i)email")}? LOCAL '@' DOMAIN {
+  System.out.println("Email: " + getText());
+};
+
+//DATETEST: DATE '/' {
+//  System.out.println("Date: " + getText());
+//};
 
 
 WS: [ \r\t\n]+ {System.out.println("matching WS rule");skip();};
--- CS319-lab8/lab8/xml.java
+++ CS319-lab8/lab8/xml.java
@@ -16,19 +16,21 @@ public class xml extends Lexer {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		ELEMENTTAG=1, WS=2;
+		STARTING=1, STOPPING=2, ELEMENTTAG=3, EMAIL=4, WS=5;
 	public static String[] modeNames = {
 		"DEFAULT_MODE"
 	};
 
 	public static final String[] ruleNames = {
-		"DIGIT", "NUMBER", "ALPHA", "ELEMENTNAME", "ELEMENTTAG", "WS"
+		"DIGIT", "ALPHA", "SPECIALCHAR", "ELEMENTNAME", "LOCAL", "DOMAIN", "DATE", 
+		"STARTING", "STOPPING", "ELEMENTTAG", "EMAIL", "WS"
 	};
 
 	private static final String[] _LITERAL_NAMES = {
+		null, null, "'>'"
 	};
 	private static final String[] _SYMBOLIC_NAMES = {
-		null, "ELEMENTTAG", "WS"
+		null, "STARTING", "STOPPING", "ELEMENTTAG", "EMAIL", "WS"
 	};
 	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
 
@@ -64,7 +66,8 @@ public class xml extends Lexer {
 	}
 
 
-	  int total = 0;
+	  boolean tag = false;
+	  String tagName = "";
 
 
 	public xml(CharStream input) {
@@ -90,71 +93,136 @@ public class xml extends Lexer {
 	@Override
 	public void action(RuleContext _localctx, int ruleIndex, int actionIndex) {
 		switch (ruleIndex) {
-		case 4:
+		case 7:
+			STARTING_action((RuleContext)_localctx, actionIndex);
+			break;
+		case 8:
+			STOPPING_action((RuleContext)_localctx, actionIndex);
+			break;
+		case 9:
 			ELEMENTTAG_action((RuleContext)_localctx, actionIndex);
 			break;
-		case 5:
+		case 10:
+			EMAIL_action((RuleContext)_localctx, actionIndex);
+			break;
+		case 11:
 			WS_action((RuleContext)_localctx, actionIndex);
+			break;
+		}
+	}
+	private void STARTING_action(RuleContext _localctx, int actionIndex) {
+		switch (actionIndex) {
+		case 0:
+			tag = true;
+			break;
+		case 1:
+			tag = true;
+			break;
+		}
+	}
+	private void STOPPING_action(RuleContext _localctx, int actionIndex) {
+		switch (actionIndex) {
+		case 2:
+			tag = false;
 			break;
 		}
 	}
 	private void ELEMENTTAG_action(RuleContext _localctx, int actionIndex) {
 		switch (actionIndex) {
-		case 0:
+		case 3:
 
 
-			  String output = getText().substring(1, getText().length() - 1);
-			  System.out.println("Starting Element Name: " + output);
+			  tagName = getText();
+			  System.out.println("Element Name: " + getText());
 
 
 			break;
-		case 1:
+		}
+	}
+	private void EMAIL_action(RuleContext _localctx, int actionIndex) {
+		switch (actionIndex) {
+		case 4:
 
-
-			  output = getText().substring(2, getText().length() - 1);
-			  System.out.println("Stopping Element Name: " + output);
-
+			  System.out.println("Email: " + getText());
 
 			break;
 		}
 	}
 	private void WS_action(RuleContext _localctx, int actionIndex) {
 		switch (actionIndex) {
-		case 2:
+		case 5:
 			System.out.println("matching WS rule");skip();
 			break;
 		}
 	}
+	@Override
+	public boolean sempred(RuleContext _localctx, int ruleIndex, int predIndex) {
+		switch (ruleIndex) {
+		case 9:
+			return ELEMENTTAG_sempred((RuleContext)_localctx, predIndex);
+		case 10:
+			return EMAIL_sempred((RuleContext)_localctx, predIndex);
+		}
+		return true;
+	}
+	private boolean ELEMENTTAG_sempred(RuleContext _localctx, int predIndex) {
+		switch (predIndex) {
+		case 0:
+			return tag;
+		}
+		return true;
+	}
+	private boolean EMAIL_sempred(RuleContext _localctx, int predIndex) {
+		switch (predIndex) {
+		case 1:
+			return !tag && tagName.matches("(?i)email");
+		}
+		return true;
+	}
 
 	public static final String _serializedATN =
-		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\2\4_\b\1\4\2\t\2\4"+
-		"\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\3\2\3\2\3\3\6\3\23\n\3\r\3\16\3"+
-		"\24\3\4\3\4\3\5\3\5\3\5\3\5\3\5\3\5\5\5\37\n\5\3\5\3\5\3\5\7\5$\n\5\f"+
-		"\5\16\5\'\13\5\5\5)\n\5\5\5+\n\5\3\5\3\5\3\5\5\5\60\n\5\3\5\3\5\3\5\7"+
-		"\5\65\n\5\f\5\16\58\13\5\5\5:\n\5\3\5\5\5=\n\5\3\5\3\5\3\5\7\5B\n\5\f"+
-		"\5\16\5E\13\5\5\5G\n\5\3\6\3\6\5\6K\n\6\3\6\3\6\3\6\3\6\3\6\3\6\5\6S\n"+
-		"\6\3\6\3\6\5\6W\n\6\3\7\6\7Z\n\7\r\7\16\7[\3\7\3\7\2\2\b\3\2\5\2\7\2\t"+
-		"\2\13\3\r\4\3\2\n\3\2\62;\4\2C\\c|\b\2/\60CMO\\aacmo|\4\2/\60aa\4\2ZZ"+
-		"zz\b\2/\60CNP\\aacnp|\7\2CY[\\aacy{|\5\2\13\f\17\17\"\"p\2\13\3\2\2\2"+
-		"\2\r\3\2\2\2\3\17\3\2\2\2\5\22\3\2\2\2\7\26\3\2\2\2\tF\3\2\2\2\13V\3\2"+
-		"\2\2\rY\3\2\2\2\17\20\t\2\2\2\20\4\3\2\2\2\21\23\5\3\2\2\22\21\3\2\2\2"+
-		"\23\24\3\2\2\2\24\22\3\2\2\2\24\25\3\2\2\2\25\6\3\2\2\2\26\27\t\3\2\2"+
-		"\27\b\3\2\2\2\30+\7z\2\2\31\32\7Z\2\2\32+\7o\2\2\33(\7O\2\2\34\37\5\3"+
-		"\2\2\35\37\t\4\2\2\36\34\3\2\2\2\36\35\3\2\2\2\37%\3\2\2\2 $\5\3\2\2!"+
-		"$\t\5\2\2\"$\5\7\4\2# \3\2\2\2#!\3\2\2\2#\"\3\2\2\2$\'\3\2\2\2%#\3\2\2"+
-		"\2%&\3\2\2\2&)\3\2\2\2\'%\3\2\2\2(\36\3\2\2\2()\3\2\2\2)+\3\2\2\2*\30"+
-		"\3\2\2\2*\31\3\2\2\2*\33\3\2\2\2+G\3\2\2\2,9\t\6\2\2-\60\5\3\2\2.\60\t"+
-		"\7\2\2/-\3\2\2\2/.\3\2\2\2\60\66\3\2\2\2\61\65\5\3\2\2\62\65\t\5\2\2\63"+
-		"\65\5\7\4\2\64\61\3\2\2\2\64\62\3\2\2\2\64\63\3\2\2\2\658\3\2\2\2\66\64"+
-		"\3\2\2\2\66\67\3\2\2\2\67:\3\2\2\28\66\3\2\2\29/\3\2\2\29:\3\2\2\2:G\3"+
-		"\2\2\2;=\t\b\2\2<;\3\2\2\2=C\3\2\2\2>B\5\3\2\2?B\t\5\2\2@B\5\7\4\2A>\3"+
-		"\2\2\2A?\3\2\2\2A@\3\2\2\2BE\3\2\2\2CA\3\2\2\2CD\3\2\2\2DG\3\2\2\2EC\3"+
-		"\2\2\2F*\3\2\2\2F,\3\2\2\2F<\3\2\2\2G\n\3\2\2\2HJ\7>\2\2IK\5\t\5\2JI\3"+
-		"\2\2\2JK\3\2\2\2KL\3\2\2\2LM\7@\2\2MW\b\6\2\2NO\7>\2\2OP\7\61\2\2PR\3"+
-		"\2\2\2QS\5\t\5\2RQ\3\2\2\2RS\3\2\2\2ST\3\2\2\2TU\7@\2\2UW\b\6\3\2VH\3"+
-		"\2\2\2VN\3\2\2\2W\f\3\2\2\2XZ\t\t\2\2YX\3\2\2\2Z[\3\2\2\2[Y\3\2\2\2[\\"+
-		"\3\2\2\2\\]\3\2\2\2]^\b\7\4\2^\16\3\2\2\2\25\2\24\36#%(*/\64\669<ACFJ"+
-		"RV[\5\3\6\2\3\6\3\3\7\4";
+		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\2\7\u008e\b\1\4\2\t"+
+		"\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13"+
+		"\t\13\4\f\t\f\4\r\t\r\3\2\3\2\3\3\3\3\3\4\3\4\3\5\3\5\3\5\3\5\3\5\3\5"+
+		"\5\5(\n\5\3\5\3\5\3\5\7\5-\n\5\f\5\16\5\60\13\5\5\5\62\n\5\5\5\64\n\5"+
+		"\3\5\3\5\3\5\5\59\n\5\3\5\3\5\3\5\7\5>\n\5\f\5\16\5A\13\5\5\5C\n\5\3\5"+
+		"\5\5F\n\5\3\5\3\5\3\5\7\5K\n\5\f\5\16\5N\13\5\5\5P\n\5\3\6\3\6\3\6\5\6"+
+		"U\n\6\3\6\3\6\3\6\5\6Z\n\6\3\6\3\6\3\6\3\6\5\6`\n\6\7\6b\n\6\f\6\16\6"+
+		"e\13\6\3\7\6\7h\n\7\r\7\16\7i\3\b\3\b\3\b\3\b\3\b\5\bq\n\b\3\t\3\t\3\t"+
+		"\3\t\3\t\3\t\5\ty\n\t\3\n\3\n\3\n\3\13\3\13\3\13\3\13\3\f\3\f\3\f\3\f"+
+		"\3\f\3\f\3\r\6\r\u0089\n\r\r\r\16\r\u008a\3\r\3\r\2\2\16\3\2\5\2\7\2\t"+
+		"\2\13\2\r\2\17\2\21\3\23\4\25\5\27\6\31\7\3\2\20\3\2\62;\4\2C\\c|\t\2"+
+		"##&&(/<=??aa\u0080\u0080\b\2/\60CMO\\aacmo|\4\2/\60aa\4\2ZZzz\b\2/\60"+
+		"CNP\\aacnp|\7\2CY[\\aacy{|\6\2\60\60\62;C\\c|\3\2\63;\3\2\63\64\3\2\65"+
+		"\65\3\2\62\63\5\2\13\f\17\17\"\"\u00a4\2\21\3\2\2\2\2\23\3\2\2\2\2\25"+
+		"\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\3\33\3\2\2\2\5\35\3\2\2\2\7\37\3\2"+
+		"\2\2\tO\3\2\2\2\13T\3\2\2\2\rg\3\2\2\2\17p\3\2\2\2\21x\3\2\2\2\23z\3\2"+
+		"\2\2\25}\3\2\2\2\27\u0081\3\2\2\2\31\u0088\3\2\2\2\33\34\t\2\2\2\34\4"+
+		"\3\2\2\2\35\36\t\3\2\2\36\6\3\2\2\2\37 \t\4\2\2 \b\3\2\2\2!\64\7z\2\2"+
+		"\"#\7Z\2\2#\64\7o\2\2$\61\7O\2\2%(\5\3\2\2&(\t\5\2\2\'%\3\2\2\2\'&\3\2"+
+		"\2\2(.\3\2\2\2)-\5\3\2\2*-\t\6\2\2+-\5\5\3\2,)\3\2\2\2,*\3\2\2\2,+\3\2"+
+		"\2\2-\60\3\2\2\2.,\3\2\2\2./\3\2\2\2/\62\3\2\2\2\60.\3\2\2\2\61\'\3\2"+
+		"\2\2\61\62\3\2\2\2\62\64\3\2\2\2\63!\3\2\2\2\63\"\3\2\2\2\63$\3\2\2\2"+
+		"\64P\3\2\2\2\65B\t\7\2\2\669\5\3\2\2\679\t\b\2\28\66\3\2\2\28\67\3\2\2"+
+		"\29?\3\2\2\2:>\5\3\2\2;>\t\6\2\2<>\5\5\3\2=:\3\2\2\2=;\3\2\2\2=<\3\2\2"+
+		"\2>A\3\2\2\2?=\3\2\2\2?@\3\2\2\2@C\3\2\2\2A?\3\2\2\2B8\3\2\2\2BC\3\2\2"+
+		"\2CP\3\2\2\2DF\t\t\2\2ED\3\2\2\2FL\3\2\2\2GK\5\3\2\2HK\t\6\2\2IK\5\5\3"+
+		"\2JG\3\2\2\2JH\3\2\2\2JI\3\2\2\2KN\3\2\2\2LJ\3\2\2\2LM\3\2\2\2MP\3\2\2"+
+		"\2NL\3\2\2\2O\63\3\2\2\2O\65\3\2\2\2OE\3\2\2\2P\n\3\2\2\2QU\5\3\2\2RU"+
+		"\5\5\3\2SU\5\7\4\2TQ\3\2\2\2TR\3\2\2\2TS\3\2\2\2Uc\3\2\2\2VZ\5\3\2\2W"+
+		"Z\5\5\3\2XZ\5\7\4\2YV\3\2\2\2YW\3\2\2\2YX\3\2\2\2Zb\3\2\2\2[_\7\60\2\2"+
+		"\\`\5\3\2\2]`\5\5\3\2^`\5\7\4\2_\\\3\2\2\2_]\3\2\2\2_^\3\2\2\2`b\3\2\2"+
+		"\2aY\3\2\2\2a[\3\2\2\2be\3\2\2\2ca\3\2\2\2cd\3\2\2\2d\f\3\2\2\2ec\3\2"+
+		"\2\2fh\t\n\2\2gf\3\2\2\2hi\3\2\2\2ig\3\2\2\2ij\3\2\2\2j\16\3\2\2\2kq\t"+
+		"\13\2\2lm\t\f\2\2mq\t\2\2\2no\t\r\2\2oq\t\16\2\2pk\3\2\2\2pl\3\2\2\2p"+
+		"n\3\2\2\2q\20\3\2\2\2rs\7>\2\2st\7\61\2\2tu\3\2\2\2uy\b\t\2\2vw\7>\2\2"+
+		"wy\b\t\3\2xr\3\2\2\2xv\3\2\2\2y\22\3\2\2\2z{\7@\2\2{|\b\n\4\2|\24\3\2"+
+		"\2\2}~\6\13\2\2~\177\5\t\5\2\177\u0080\b\13\5\2\u0080\26\3\2\2\2\u0081"+
+		"\u0082\6\f\3\2\u0082\u0083\5\13\6\2\u0083\u0084\7B\2\2\u0084\u0085\5\r"+
+		"\7\2\u0085\u0086\b\f\6\2\u0086\30\3\2\2\2\u0087\u0089\t\17\2\2\u0088\u0087"+
+		"\3\2\2\2\u0089\u008a\3\2\2\2\u008a\u0088\3\2\2\2\u008a\u008b\3\2\2\2\u008b"+
+		"\u008c\3\2\2\2\u008c\u008d\b\r\7\2\u008d\32\3\2\2\2\31\2\',.\61\638=?"+
+		"BEJLOTY_acipx\u008a\b\3\t\2\3\t\3\3\n\4\3\13\5\3\f\6\3\r\7";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
